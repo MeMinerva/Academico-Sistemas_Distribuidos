48475c65ec54249306fcd61686ce775499f1aea3
Minerva <mariaminervaccomp@gmail.com>
2025-06-04 05:25:42 -0300

    top

--- Trabalho prático/front-end/main.js
+++ Trabalho prático/front-end/main.js
@@ -3,92 +3,75 @@ const path = require('path');
 
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
-            width: 1050,
-            height: 700,
-            minHeight: 700,
-            minWidth: 1050,
-            webPreferences: {
-                preload: path.join(__dirname, 'preload.js'),
-                nodeIntegration: true,
-                contextIsolation: false
-            }
-        });
-        Menu.setApplicationMenu(null);
-        mainWindow.loadFile('app/index.html');
-    }
+        width: 1050,
+        height: 700,
+        minHeight: 700,
+        minWidth: 1050,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+    Menu.setApplicationMenu(null);
+    mainWindow.loadFile('app/index.html');
+}
 
-
-
-ipcMain.on('abrir-nova-conversa', () => {
-    const novaConversaWindow = new BrowserWindow({
+ipcMain.on('init-chat', () => {
+    const initGrChatWindow = new BrowserWindow({
         width: 300,
         height: 375,
         resizable: false,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: true,
-                    // webSecurity: false // desabilita CORS e outras proteções (apenas para teste)
-
         },
-        alwaysOnTop: true, // opcional: para ficar sobre a janela principal
-        frame: false,       // ou false se for popup tipo modal
+        alwaysOnTop: true,
+        frame: false,
         show: false,
-        opacity: 0.95        // vibrancy: 'under-window'    
-});
-
-    Menu.setApplicationMenu(null);
-
-    novaConversaWindow.loadFile('app/html/nova-conversa.html');
-
-    novaConversaWindow.once('ready-to-show', () => {
-        novaConversaWindow.show();
+        opacity: 0.95
     });
 
-    // Fecha quando perder o foco (clicou fora)
-    novaConversaWindow.on('blur', () => {
-        if (!novaConversaWindow.webContents.isDevToolsOpened()) {
-            novaConversaWindow.close();
+    Menu.setApplicationMenu(null);
+    initGrChatWindow.loadFile('app/html/nova-conversa.html');
+    initGrChatWindow.once('ready-to-show', () => {
+        initGrChatWindow.show();
+    });
+    initGrChatWindow.on('blur', () => {
+        if (!initGrChatWindow.webContents.isDevToolsOpened()) {
+            initGrChatWindow.close();
         }
     });
 });
 
-ipcMain.on('abrir-novo-grupo', () => {
-    const novaConversaWindow = new BrowserWindow({
+ipcMain.on('init-group-chat', () => {
+    const initGrChatWindow = new BrowserWindow({
         width: 320,
         height: 375,
         resizable: false,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: true,
-                    // webSecurity: false // desabilita CORS e outras proteções (apenas para teste)
-
         },
-        alwaysOnTop: true, // opcional: para ficar sobre a janela principal
-        frame: false,       // ou false se for popup tipo modal
+        alwaysOnTop: true,
+        frame: false,
         show: false,
-        opacity: 0.95        // vibrancy: 'under-window'    
-});
-
-    Menu.setApplicationMenu(null);
-
-    novaConversaWindow.loadFile('app/html/novo_grupo.html');
-
-    novaConversaWindow.once('ready-to-show', () => {    
-        novaConversaWindow.show();
+        opacity: 0.95
     });
-
-    // Fecha quando perder o foco (clicou fora)
-    novaConversaWindow.on('blur', () => {
-        if (!novaConversaWindow.webContents.isDevToolsOpened()) {
-            novaConversaWindow.close();
+    Menu.setApplicationMenu(null);
+    initGrChatWindow.loadFile('app/html/novo_grupo.html');
+    initGrChatWindow.once('ready-to-show', () => {
+        initGrChatWindow.show();
+    });
+    initGrChatWindow.on('blur', () => {
+        if (!initGrChatWindow.webContents.isDevToolsOpened()) {
+            initGrChatWindow.close();
         }
     });
 });
 
-
 app.whenReady().then(() => {
     createMainWindow();
-
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
     });
